375d0301625c046b987363770c9aa2fed12c9f0b
corpoinsular <insulardigital@gmail.com>
2026-06-15 15:17:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -30,20 +30,3 @@ document.querySelectorAll(".btn-detalle").forEach(btn => {
 document.querySelector(".cerrar-detalle")?.addEventListener("click", () => {
     detalle.classList.remove("active");
 });
-
-<script>
-let slides = document.querySelectorAll(".slide");
-let index = 0;
-
-function showSlide(i){
-    slides.forEach(s => s.classList.remove("active"));
-    slides[i].classList.add("active");
-}
-
-function nextSlide(){
-    index = (index + 1) % slides.length;
-    showSlide(index);
-}
-
-setInterval(nextSlide, 4000); // cada 4 segundos
-</script>
